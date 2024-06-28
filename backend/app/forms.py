db52f7edd31908d6afbf5ce74003b11f39032b27
VickyThawne <vanshj.prajapati@gmail.com>
2024-06-28 13:25:45 +0530

    changes with upload checks

--- backend/app/forms.py
+++ backend/app/forms.py
@@ -3,5 +3,6 @@ from flask_wtf.file import FileField, FileAllowed
 from wtforms import SubmitField
 
 class UploadForm(FlaskForm):
-    image = FileField('Upload Image', validators=[FileAllowed(['jpg', 'png'])])
+    image = FileField('Upload Image', validators=[FileAllowed(['jpg', 'png', 'jpeg'])])
     submit = SubmitField('Upload')
+
--- backend/app/models.py
+++ backend/app/models.py
@@ -3,6 +3,7 @@ from app import db
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(120), unique=True, nullable=False)
+    filepath = db.Column(db.String(120))
     upload_time = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
