a32401e7b2a76cb2286836efbd6202605fadbde4
pranavpatel7260 <pranavpatel7260@gmail.com>
2024-06-29 06:27:34 +0000

    merge branch

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,9 +1,45 @@
 from app import db
 
 class Image(db.Model):
+    __tablename__ = "image_description"
+
     id = db.Column(db.Integer, primary_key=True)
     filename = db.Column(db.String(120), unique=True, nullable=False)
+    filepath = db.Column(db.String(120))
     upload_time = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
         return f"Image('{self.filename}', '{self.upload_time}')"
+
+class ImageInformation(db.Model):
+    __tablename__ = 'image_information'
+
+    id = db.Column(db.Integer, primary_key=True)
+    worker_details_id = db.Column(db.Integer, nullable=False)
+    body_part_name = db.Column(db.String(255), nullable=False)
+    kp_x = db.Column(db.Float, nullable=True)
+    kp_y = db.Column(db.Float, nullable=True)
+    image_path = db.Column(db.String(255), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    gender = db.Column(db.String(50), nullable=True)
+    worker_present = db.Column(db.Boolean, nullable=False)
+    worker_count = db.Column(db.Integer, nullable=False)
+    ppe_worn = db.Column(db.String(50))
+    visible_tools = db.Column(db.String(50))
+    environment_type = db.Column(db.String(50))
+    back_position = db.Column(db.String(50))
+    neck_position = db.Column(db.String(50))
+    arm_position = db.Column(db.String(50))
+    leg_position = db.Column(db.String(50))
+    lifting_status = db.Column(db.Boolean, nullable=False)
+    repetitive_motion = db.Column(db.Boolean, nullable=False)
+    work_height = db.Column(db.String(50))
+    balance_status = db.Column(db.String(50))
+    work_surface = db.Column(db.String(50))
+    ergonomic_risk_level = db.Column(db.String(50))
+    fatigue_indicators = db.Column(db.Boolean, nullable=False)
+    cumulative_risk_score = db.Column(db.Integer, nullable=False)
+    max_strain_in_part = db.Column(db.String(50))
+
+    def __repr__(self):
+        return f'<ErgonomicAssessment {self.id}>'
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,43 +1,33 @@
-import os
-from datetime import datetime
-from flask import render_template, url_for, flash, redirect, request, send_from_directory
-from werkzeug.utils import secure_filename
 from app import app, db
 from app.forms import UploadForm
 from app.models import Image
-from app.vision_model import load_model, process_image
+# from app.vision_model import load_model, process_image
+from app.utils import save_image
 
-model = load_model()
+# model = load_model()
 
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    form = UploadForm()
-    return render_template('home.html', form=form)
-
-@app.route('/upload', methods=['POST'])
-def upload():
     form = UploadForm()
     if form.validate_on_submit():
         image_file = form.image.data
         filename = secure_filename(image_file.filename)
         upload_path = os.path.join(app.root_path, 'static/uploads', filename)
         image_file.save(upload_path)
-        image = Image(filename=filename, upload_time=datetime.utcnow())
+        image = Image(filename=filename, filepath=upload_path, upload_time=datetime.utcnow())
         db.session.add(image)
         db.session.commit()
-        output_path = process_image(upload_path, model)
         return redirect(url_for('result', filename=filename))
     return render_template('home.html', form=form)
 
-@app.route('/result/<filename>')
-def result(filename):
-    return render_template('result.html', filename=filename)
-
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(os.path.join(app.root_path, 'static/uploads'), filename)
-
-@app.route('/outputs/<filename>')
-def output_file(filename):
-    return send_from_directory(os.path.join(app.root_path, 'static/outputs'), filename)
+@app.route('/upload', methods=['POST'])
+	@@ -23,10 +32,10 @@ def upload():
+        filename = secure_filename(image_file.filename)
+        upload_path = os.path.join(app.root_path, 'static/uploads', filename)
+        image_file.save(upload_path)
+        image = Image(filename=filename, filepath=upload_path, upload_time=datetime.utcnow())
+        db.session.add(image)
+        db.session.commit()
+        output_path = process_image(upload_path)
+        return redirect(url_for('result', filename=filename))
+    return render_template('home.html', form=form)
